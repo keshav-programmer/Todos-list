a1f10e39beb86dc994aecc7f9c6c80b7d571cab8
Keshav <alkaguptasanjay@gmail.com>
2021-10-14 09:29:40 -0400

    initial commit

---
+++ src/My Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is a About Component
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, soluta. Consequatur nulla vel incidunt consectetur quia? Debitis voluptatibus recusandae saepe perferendis sit exercitationem, unde rerum dignissimos ullam neque laboriosam repellat aliquam, nesciunt necessitatibus, nulla amet. Est deserunt explicabo recusandae? Impedit veritatis dolorem doloremque repellat sit quos nobis a nihil, porro perspiciatis velit fugiat ipsa omnis ullam eum quaerat voluptatum sint.</p>
+        </div>
+    )
+}
---
+++ src/My Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+    
+    return (
+        <div>
+         <footer className="bg-dark text-light py-3" >
+             <p className="text-center">
+                 Copyright&copy; all Right reserved to Keshav Gupta
+             </p>
+         </footer>
+        </div>
+    )
+}
